d559beb7193d8c19ccc97957faa0ab6e4f1bffe7
Hari-hub-04 <rameshhari986@gmail.com>
2025-07-15 12:21:12 +0530

    chore: js basic

---
+++ variables.js
@@ -0,0 +1,12 @@
+//variables
+
+/*there are 3 types of variables
+1. var -- it is an old way of declaring variable
+2. let -- it is a modern way of declaring variable
+3. const -- it is used to declare a constant variable (name,date of birth)*/
+
+let age = 20;
+const names = "Hari";
+let degree = "BE";
+const gender = "male";
+console.log("My name is "+ names+ " I'm"+age+"years old, Im studying"+degree);
